a80c009883e2032b872a5f3788e440a0bc63f0e3
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-07-03 16:24:24 -0500

    Create baumann-assignment-1.4.js

---
+++ web-420/baumann-assignment-1.4.js
@@ -0,0 +1,25 @@
+{
+    "first_name": "Bruce",
+    "last_name": "Wayne",
+    "birthday": "April 7, 1930",
+    "roles": [
+        {
+          "title": "Billionaire"
+        },
+        {
+            "title": "Billionaire"
+        }
+ 
+    ],
+    "dependents": [
+      {
+          "first_name": "Jason",
+          "last_name": "Todd"
+      },
+      {
+          "first_name": "Damien",
+          "last_name": "Al Ghul"
+      }
+ 
+  ]
+ }
